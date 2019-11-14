752182931fcc3fd2b3578c1526e5f797765deb34
Satrio Adhi <satrioadhi04@gmail.com>
2019-11-14 15:30:56 +0700

    Front end commit

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue' 
+import Router from 'vue-router' 
+ 
+const DashboardLayout = () => import(/* webpackChunkName: "dashboard" */ '../components/dashboardLayout.vue') 
+ 
+function loadView(view) {  
+    return () => import(/* webpackChunkName: "view[request]" */ 
+    `../components/dashboardContents/${view}.vue`) 
+} 
+ 
+const routes = [     
+    {       
+        path: '/',       
+        component: DashboardLayout,       
+        children: [         
+            {           
+                name: 'UserController',           
+                path: '',           
+                component: loadView('userController')         
+            },
+            {           
+                name: 'LayananKendaraanController',           
+                path: '/layanankendaraan',           
+                component: loadView('LayananKendaraanController')         
+            }       
+        ]     
+    },   
+]   
+
+Vue.use(Router) 
+ 
+const router = new Router({mode: 'history', routes: routes}) 
+
+export default router   
